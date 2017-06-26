c176ec0ef25f65decd1b1d2ac2deb9457fa9e009
Aisling <a.guinan3@nuigalway.ie>
2017-06-27 00:04:34 +0100

    Save progress, need to fix images (preloading?) and animation on hover poss?

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	console.log('ready');
+	inView.offset(300);
+	inView('.projects').on('enter', function(element){
+		// console.log('in view', element)
+		$(element).removeClass('invisible');
+	   	$(element).addClass('animated bounceInDown');
+	   
+	})
+	.on('exit', function(element){
+		// console.log('exit', element)
+		setTimeout(function(){ 
+		}, 2000);
+	});
+});
