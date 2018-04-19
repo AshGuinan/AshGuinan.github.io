04ecfb6977d313b1eaf3f9b7fff86fd0a4042b5f
Ais <a.guinan3@nuigalway.ie>
2018-04-19 22:43:24 +0100

    WIP - Updates to the portfolio site

--- scripts/main.js
+++ scripts/main.js
@@ -11,30 +11,53 @@
 
 		for(var x = 0; x <= weatherReport.weather.length-1; x++){
 			res.push(x);
+			// weatherReport.weather[0].main
 			switch (weatherReport.weather[0].main) {
 				case 'Thunderstorm':
 				console.log('Storm!');
+				// dark sky + dark clouds
+				//Flashing animation
+				$(".cloud").addClass("dark");
+				$(".sky").addClass("darkSky");
+				$(".sun").addClass("hidden");
 				break;
 				case 'Drizzle':
 				console.log('Drizzle!');
+				// blue sky & dark clouds
+				$(".sky").addClass("darkSky");
+				// $(".sky").removeClass("sky");
 				break;
 				case 'Rain':
 				console.log('Rain!');
+				//dark clouds & sky
+				$(".cloud").addClass("dark");
+				$(".sun").addClass("hidden");
+				$(".sky").addClass("darkSky");
+				// $(".sky").removeClass("sky");
 				break;
 				case 'Snow':
 				console.log('Snow!');
+				$(".cloud").addClass("dark");
+				$(".sky").addClass("darkSky");
+				// $(".sky").removeClass("sky");
 				break;
 				case 'Atmosphere':
 				console.log('Some real atmosphere!');
 				break;
 				case 'Clear':
 				console.log('Clear!');
+				// no clouds, blue sky
+				$(".cloud").removeClass("cloud");
 				break;
 				case 'Clouds':
 				console.log('Some clouds!');
+				//clear sky, white clouds
+				$(".sun").addClass("hidden");
 				break;
 				case 'Extreme':
 				console.log('Blown Away!');
+				//dark sky, dark clouds
+				$(".cloud").addClass("dark");
 				break;
 				case 'Additional':
 				console.log('Bonus!');
@@ -46,7 +69,7 @@
 			}
 		}
 
-		$('#temperature').text("Looks like we're having " + weatherReport.weather[0].description + " in Galway at the moment!");
+		$('#temperature').text("Looks like we're having a " + weatherReport.weather[0].description + " in " + data.q + " at the moment!");
 	}
 
 
@@ -60,7 +83,6 @@
 
 		$.preloadImages("images/jez-timms-83901.jpg","images/catsanddogs_Hiura_Flour.png", "images/william-iven-19844.jpg");
 
-		
 		console.log('ready');
 		inView.offset(300);
 		inView('.projects').on('enter', function(element){
@@ -69,14 +91,38 @@
 		   	//$(element).addClass('animated bounceInDown');
 		   	$(element).hide().fadeIn(1000);
 		   	$('.hidden').fadeIn(1500).removeClass('hidden');
-
-	   })
-		.on('exit', function(element){
+	  }).on('exit', function(element){
 		// console.log('exit', element)
 			setTimeout(function(){ 
-		}, 2000);
-	});
-		
+			}, 2000);
+		});
+	
+		var $el, $ps, $up, totalHeight;
+
+		$(".skills .button").click(function() {
+		      
+
+		  $el = $(this);
+		  $p  = $el.parent();
+		  $up = $p.parent();
+		  $ps = $up.find("p:not('.continue')");
+		  
+		  totalHeight = $up[0].scrollHeight
+		  $up.css({
+	      // Set height to prevent instant jumpdown when max height is removed
+	      "height": $up.height(),
+	      "max-height": 9999
+	    }).animate({
+	      "height": totalHeight
+	    });
+		  
+		  // fade out read-more
+		  $p.fadeOut();
+		  
+		  // prevent jump-down
+		  return false;
+		    
+		});
 	});
 })(jQuery);
 
