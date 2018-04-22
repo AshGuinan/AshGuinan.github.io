5067a14463bcf52a8bee628137d0b75a342a0a95
Aisling Guinan <Aisling Guinan>
2018-04-22 20:36:41 +0100

    Updates!

--- scripts/main.js
+++ scripts/main.js
@@ -87,19 +87,18 @@
 
 		$.preloadImages("images/jez-timms-83901.jpg","images/catsanddogs_Hiura_Flour.png", "images/william-iven-19844.jpg");
 
-		console.log('ready');
-		inView.offset(300);
-		inView('.projects').on('enter', function(element){
-			// console.log('in view', element)
-			$(element).removeClass('invisible');
-		   	//$(element).addClass('animated bounceInDown');
-		   	$(element).hide().fadeIn(1000);
-		   	$('.hidden').fadeIn(1500).removeClass('hidden');
-	  }).on('exit', function(element){
-		// console.log('exit', element)
-			setTimeout(function(){ 
-			}, 2000);
-		});
+		// inView.offset(300);
+		// inView('.projects').on('enter', function(element){
+		// 	// console.log('in view', element)
+		// 	// $(element).removeClass('invisible');
+		//    	//$(element).addClass('animated bounceInDown');
+		//    	$(element).hide().fadeIn(1000);
+		//    	$('.projects').fadeIn(1500).removeClass('hidden');
+	 //  }).on('exit', function(element){
+		// // console.log('exit', element)
+		// 	setTimeout(function(){ 
+		// 	}, 2000);
+		// });
 	
 		var $el, $ps, $up, totalHeight;
 
